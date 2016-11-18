0b4dd56ca90d417fd5063f1635be98e0f39b7a0a
Yang <ly.live@yahoo.com>
2016-11-17 17:50:16 -0800

    Update modal.js

--- modules/modal/scripts/modal.js
+++ modules/modal/scripts/modal.js
@@ -18,17 +18,20 @@
     'use strict';
 
     var Modal = function(element, options) {
-        this.init("Modal", element, options);
+        this.init("modal", element, options);
     }
 
     // Default options define
-    Modal.VERSION = "1.2.4";
+    Modal.VERSION = "1.1.0";
     Modal.SELECTORS = {
-        modalSelector: ".modal",
-        modalBoxSelector: ".modal-box",
+        toggleSelector: "[data-toggle='modal']",
+        modalSelector: ".nmodal",
+        closeSelector: ".nmodal-close",
+        bodySelector: ".nmodal-box-body",
+        modalBoxSelector: ".nmodal-box",
         maskSelector: "#overlay"
     };
-    
+
     Modal.TRANSITION_DURATION = 150;
     Modal.BACKDROP_TRANSITION_DURATION = 100;
 
@@ -40,57 +43,67 @@
     //Initialization
     Modal.fn.init = function(type, element, options) {
         var ths = this;
+        this.$el = $(element);
 
         //Option priority: data - options > incoming - options > default -options
-        this.options = $.extend({}, Modal.DEFAULTS, Modal.SELECTORS, options);
-
+        this.options = $.extend({}, Modal.DEFAULTS, Modal.SELECTORS, options, ths.$el.data());
         this.type = type || this.options.type;
-        this.$element = $(element);
-        this.$source = this.getSource();
-        this.globalState = {
-            scroll: true,
-            resize: true
-        };
 
-        // Trigger init event
-        this.trigger(type, this.options.toggleSelector);
+        this.$target = $(this.options.target).data("n.modal", this);
+        this.$box = $(this.options.modalBoxSelector, this.$target);
+        this.$close = $(this.options.closeSelector, this.$target);
+        this.$body  =  $(this.options.bodySelector, this.$target);
+        this.$mask = $(this.options.maskSelector);
+
+        // box event stop
+        this.$box.click(function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+        });
+        // close event
+        this.$close.click(function(){
+            ths.close();
+        });
+        // mask event
+        this.$target.on("click",function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            ths.close();
+            return false;
+        });
+
+        if (this.options.remote) {
+         this.$body.load(this.options.remote, $.proxy(function () {
+              this.$el.trigger('loaded.n.modal')
+            }, this))
+        }
     };
-
-    //Trigger bind
-    Modal.fn.trigger = function(type, selector) {
-
-    }
 
     // Event
-    Modal.fn.toggle = function() {};
+    Modal.fn.toggle = function() {
+        var ths = this;
 
-    // Set position
-    Modal.fn.setPosition = function() {};
-
-    //Set position
-    Modal.fn.setArrow = function() {};
-
-    // Set effect
-    Modal.fn.setEffect = function($target, json) {}
-        //Get mask
-    Modal.fn.getMask = function() {};
-
-    // Get Target
-    Modal.fn.getTarget = function() {}
-
-    //leave event
-    Modal.fn.leave = function() {
-        this.hide();
+        if(this.$target.hasClass("open")){
+            this.close();
+        }else{
+            this.open();
+        }
     };
 
-    // Close Modal
-    Modal.fn.hide = function() {};
-
-    // show
-    Modal.fn.show = function() {};
-
-    //Set mask
-    Modal.fn.setMask = function() {}
+    // show();
+    Modal.fn.open = function() {
+        var ths = this;
+        this.$target.addClass("open");
+        this.$mask.addClass("open");
+        this.$el.trigger($.Event('open.n.modal'));
+    };
+    // close
+    Modal.fn.close = function() {
+        this.$target.removeClass("open");
+        this.$mask.removeClass("open");
+        this.$el.trigger($.Event('close.n.modal'));
+    };
 
     //Update
     Modal.fn.update = function() {};
@@ -107,15 +120,13 @@
     function Plugin(option) {
         return this.each(function() {
             var $this = $(this),
-                data = $this.data("ng.Modal");
+                  api = $this.data("n.modal");
 
-            options = typeof option == "object" ? $.extend({}, option, $this.data()) : $this.data();
+            if (!api && /destroy|hide/.test(option)) return;
 
-            if (!data && /destroy|hide/.test(option)) return;
+            if (!api) $this.data('n.modal', (api = new Modal(this, option)));
 
-            if (!data) $this.data('ng.Modal', (data = new Modal(this, options)));
-
-            if (typeof option == 'string') data[option]();
+            if (typeof option == 'string') api[option]();
         })
     }
 
@@ -127,19 +138,21 @@
     // POPOVER NO CONFLICT
     // ===================
     $.fn.Modal.noConflict = function() {
-        $.fn.Modal = old
-        return this
+        $.fn.Modal = old;
+        return this;
     }
 
     // POPOUT DATA-API
-    var Handler = {};
+    var Handler = {
+        toggle: function(e){
+
+            if ($(this).is('a')) e.preventDefault();
+
+            Plugin.call($(this), "toggle");
+        }
+    };
 
     $(document)
-        .on("click.n.Modal", Modal.SELECTORS.toggleSelector, Handler.toggle)
-        .on("click.n.Modal", Modal.SELECTORS.closeSelector, Handler.hide);
-
-    // $(window)
-    //     .resize(Handler.resizeUpdate)
-    //     .scroll(Handler.scrollUpdate);
+        .on("click.n.modal", Modal.SELECTORS.toggleSelector, Handler.toggle);
 
 }(window, jQuery, document);
