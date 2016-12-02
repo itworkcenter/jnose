b11975e7cfc7c2224093152297405b2a1452e93e
Yang <ly.live@yahoo.com>
2016-12-01 17:50:57 -0800

    Update mask.js

--- modules/mask/scripts/mask.js
+++ modules/mask/scripts/mask.js
@@ -2,7 +2,7 @@
  * Checkmask.js v1.0.0
  * Creator: Carl Liu
  * Date: 2016.11.28
- * Notice: Form input mask validate.
+ * Notice: Form input mask validate. Support english mode.
  * ******************************* */
 
 /* *******************************
@@ -11,7 +11,7 @@
  * ******************************* */
 
 /*
-*Collapse Useage:*
+*Checkmask Useage:*
 */
 
 +function(window, $, document, undefined) {
@@ -23,73 +23,91 @@
 
     Checkmask.fn = Checkmask.prototype ={
         init: function(element, options){
-            var self = this, $el;
-            var len;
+            var ths = this, $el;
 
             this.$el = $el = $(element);
-            //Option priority: self data> default
+            //Option priority: ths data> default
             this.options = $.extend({}, Checkmask.DEFAULTS, options, $el.data());
-            this.setCheckmask();
+            this.rules = []; //mask string  to rules array;
+            this.patterns = []; //mask string to patterns array;
+            this.len = 0; //patterns length;
+            this.writeBuffer();
 
             $el
-            .on("focus.mask", focusEvent)
-            .on("blur.mask", blurEvent)
-            .on("keydown.mask", keydownEvent)
-            .on("keypress.mask", keypressEvent);
+            .on("focus.mask", $.proxy(ths.focusEvent, ths))
+            .on("blur.mask", $.proxy(ths.blurEvent, ths))
+            .on("keydown.mask", $.proxy(ths.keydownEvent, ths))
+            .on("keypress.mask", $.proxy(ths.keypressEvent, ths));
         },
         readOnly: function(){
             return this.$el.prop("readonly");
         },
-        focusEvent: function(){
+        focusEvent: function(e){
             // Focus Event
-            if(self.readOnly()){return};
+            if(this.readOnly()){return};
 
         },
         blurEvent: function(){
             // Focus Event
-            if(self.readOnly()){return};
+            if(this.readOnly()){return};
 
         },
         keypressEvent: function(e){
             //keypress Event
-            if(self.readOnly()){return};
+            if(this.readOnly()){return};
 
 
         },
         keydownEvent: function(e){
             //keydown Event
-            if(self.readOnly()){return};
+            if(this.readOnly()){return};
+            var p, c, next, key = e.which || e.keyCode, pos = this.$el.caret();
 
 
+            var dd =this.getPatternsForInput(e.key, pos.begin-pos.end===0 ? pos.begin : pos.end);
+            this.$el.val(dd);
         },
         check: function(){
             // check holder
         },
         seekNext: function(pos){
-            var len = this.len;
-            for (;++pos < len && !tests[pos]; ) ;
+            var ths = this, len = ths.len;
+
+            for (;++pos < len && !ths.rules[pos]; ) ;
             return pos;
         },
         seekPrev: function(pos){
-            for (;--pos >= 0 && !tests[pos]; )
+            var ths = this;
+
+            for (;--pos >= 0 && !ths.rules[pos]; )
             return pos;
         },
-        setCheckmask: function(){
-            var dd,aa
+        writeBuffer: function(){
             // set Checkmask
-            this.$el.val(this.getCheckmask());
+            this.$el.val(this.getPatterns().join(""));
         },
-        getCheckmask: function(){
-            // get mask
-            var self = this;
-            var definition = this.options.definition;
-            var mask = this.options.mask;
+        setRule: function(mask_cell){
+            var define = this.options.definition[mask_cell];
+            this.rules.push(define ? new RegExp(define) : "");
+        },
+        getPatterns: function(){
+            // get Patterns
+            var ths = this,define = this.options.definition, mask = this.options.mask;
 
-            var reslt = $.map(mask.split(""), function(el, index){
-                    return definition[el] ? self.getPlaceholderIndex(index) : el;
-                }).join(" ");
-                this.mask = reslt;
-            return reslt;
+            return this.patterns = $.map(mask.split(""), function(str, i){
+                    // create regular rule array  and patterns
+                    return ths.setRule(str), define[str] ? ths.getPlaceholderIndex(i) : str;
+                });
+        },
+        getPatternsForInput: function(input, index){
+            // get Patterns
+            console.log(input +" "+ index);
+            var ths = this,define = this.options.definition, patterns = this.patterns;
+
+            patterns[ths.seekNext(index)] = input;
+            console.log(patterns);
+
+            return  patterns.join("");
         },
         getPlaceholderIndex: function(i){
             var holder = this.options.placeholder;
